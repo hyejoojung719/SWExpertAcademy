a85f2a30fd79ed074af7af68778159c4e09ccfc3
fixup <fixup719@gmail.com>
2022-04-25 10:51:11 +0900

    N2046 스탬프찍기, 코드리뷰 필요X

---
+++ D1/src/N2046.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// ½ºÅÆÇÁ Âï±â
+public class N2046 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num  = sc.nextInt();
+		
+		for(int i=0; i<num; i++) {
+			System.out.print("#");
+		}
+	}
+}
